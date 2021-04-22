c1acdbb5235094f2003f0bfe9f72a767a5bf6d84
choirull-asharii <asharisukses1999@gmail.com>
2021-04-22 12:16:02 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (-) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Choirul Asror Ashari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 4.00;
+
+}
