3e289b9e80ecf6c06cfb0e58860d363e796ce394
choirull-asharii <asharisukses1999@gmail.com>
2021-04-22 12:56:07 +0800

    percabangan switch dengan char

--- src/percabangan/Switch.java
+++ src/percabangan/Switch.java
@@ -2,22 +2,27 @@ package percabangan;
 
 public class Switch {
     public static void main(String[] args){
-        int jalanJalan=2;
-        switch(jalanJalan){
-            case 1:
-                System.out.println("Urang Pacaran");
+        char nilai='B';
+        switch(nilai){
+            case 'A':
+                System.out.println("Mhs    : \"Selamat Pagi pak\"");
+                System.out.println("Dosen  : \"iya pagi!!\"");
                 break;
-            case 2:
-                System.out.println("Urang Selingkuh");
+            case 'B':
+                System.out.println("Mhs    : \"Saya boleh bertanya pak?\"");
+                System.out.println("Dosen  : \"iya\"");
                 break;
-            case 3:
-                System.out.println("ubat nyamuk");
-                break;
-            case 4:
-                System.out.println("ubat nyamuk");
+            case 'C':
+                System.out.println("Mhs    : \"Kenapa nilai saya D ya pak?:(\"");
+                System.out.println("Dosen  : \"Apakah kamu sudah menggumpulkan tugas?\"");
+                System.out.println("Mhs    : \"Sudah pak\"");
                 break;
             default:
-                System.out.println("makhluk halus");
+                System.out.println("Mhs    : \"Kemarin saya menggumpulkan sesuai waktu yang di tentukan pak\"");
+                System.out.println("Dosen  : \"Coba saya lihat dulu>\"");
+                System.out.println("Dosen  : \"*Melihati Berkas\"");
+                System.out.println("Dosen  : \"sudah menggumpulkan namun masih di e-learning belum di kirimkan ke berkas bapak\"");
+                System.out.println("Mhs    : \"Astaga\"");
                 break;
         }
     }
