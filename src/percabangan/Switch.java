4ab19bf9e337b9e6ac39d4ca9ad1bd6bfe212112
choirull-asharii <asharisukses1999@gmail.com>
2021-04-22 12:36:38 +0800

    percabangan switch dengan int

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args){
+        int jalanJalan=2;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("Urang Pacaran");
+                break;
+            case 2:
+                System.out.println("Urang Selingkuh");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("ubat nyamuk");
+                break;
+            default:
+                System.out.println("makhluk halus");
+                break;
+        }
+    }
+}
