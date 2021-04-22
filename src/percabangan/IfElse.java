bd2c813bbaf76d0b3976a3ed3da08d9d786196f5
choirull-asharii <asharisukses1999@gmail.com>
2021-04-22 12:31:39 +0800

    percabangan IfElse dengan kondisi false statement2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,10 +3,13 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args){
         double totalBelanja = 25000;
-        double uangDiDompet = 30000;
+        double uangDiDompet = 20000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
+        }else if(uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang Cukup, angsul : "+angsul);
         }else{
             System.out.println("Uang pas... Selamat Menikmati..");
         }
